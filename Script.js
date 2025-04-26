18f565b74b44761d5a0d6a6325b780df00bb43b9
hugofellipe406 <hugofellipe-se@hotmail.com>
2025-04-26 20:49:17 -0300

    attu

--- Script.js
+++ Script.js
@@ -3,7 +3,7 @@ botao.addEventListener('mouseenter' , ()=> {
 const larguraJanela = window.innerWidth;
 const alturaJanela = window.innerHeight;
 const novaPosicaoX = Math.random() * (larguraJanela - botao.offsetWidth);
-const novaPosicaoY = Math.random() * (larguraJanela - botao.offsetHeight);
+const novaPosicaoY = Math.random() * (alturaJanela - botao.offsetHeight);
 
 botao.style.left ='${novaPosicaoX}px';
 botao.style.top = '${novaPosicaoY}px';
